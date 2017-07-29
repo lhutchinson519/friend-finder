f184397aea23f1f1c80d397452765bd9a36108fa
Lauren Hutchinson <Lauren Hutchinson>
2017-07-29 15:49:00 -0500

    comparisons difficult

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,39 +5,62 @@ var friends = require("../data/friends.js");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
+// for i in range(length(questions)):
+// 	for j in range(length(friends)):
+// 		diff = aboslute( new_person.questions[i] - friends[j].questions[i] ):
+// 			score[i] += diff
+
+
+
 module.exports = function(app) {
-        app.get("/api/friends", function(req, res) {
-            res.json(friends);
-        });
 
-        // app.get("/api/", function(req, res) {
-        //     res.json(newFriend);
-        // })
-        // Create New person - takes in JSON input
-        app.post("/api/friends", function(req, res) {
+    //get the friends page and return data
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
-                var newFriend = req.body;
+    // post the friends + the new friend and comparison to return best match
+    app.post("/api/friends", function(req, res) {
 
-                var newFriendScore = [];
-                var compareFriends = [];
+        var newFriend = req.body;
 
-                console.log(newFriend);
+        //new friend array of chosen
+        var newFriendScore = [];
 
-                for (i = 0; i < friends.length; i++){
-                	compareFriends = friends[i].scores;
-                	console.log("Friends Scores: " + compareFriends);
-                }
+        // friends in databse array of chosen
+        var compareFriends = [];
 
-                for (i = 0; i < newFriend.length; i++) {
-                    newFriendScore = newFriend[i].scores;
-                    console.log("New Friend Scores" + newFriendScore);
-                }
+        console.log(newFriend);
 
+        for (i = 0; i < friends.length; i++) {
+            compareFriends = friends[i].scores;
+            console.log("Friends Scores: " + compareFriends);
+        }
 
-                    // friends.push(newFriend);
-                    // console.log(friends);
+        for (i = 0; i < newFriend.length; i++) {
+            newFriendScore = newFriend[i].scores;
+            console.log("New Friend Scores" + newFriendScore);
+        }
 
+        // friends: 
+        // person_1 : [1, 2, 4]
+        // person_2 : [3, 3, 2]
+        // score_comparison = [0, 0]
 
-                    //res.json(newFriend);
-        });
-    };
+        //first time through loop (question 1): [0, 2]
+        //second time (question 2): [0 + 0 = 0, 1 + 2 = 3]
+        //third time (question 3): [0 + 0 + 1 = 1, 1 + 2 + 1 = 4]
+        //end : [1, 4]
+
+        //for (i = 0; i < newFriendScore.length; i++) {
+        	//console.log(newFriend[i] - compareFriends[i]);
+        	// for (i = 0; i < newFriendScore.length; i++){
+
+        	// }
+        }
+        friends.push(newFriend);
+        console.log(friends);
+        res.json(newFriend);
+    });
+};
